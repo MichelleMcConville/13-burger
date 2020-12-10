1848a17bb8749f7ccf6b4acb311578ef6abe16b4
Michelle McConville <mchelmconvl@gmail.com>
2020-12-10 14:31:39 -0600

    added express configuration

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm package(s) that will be used to give server useful functionality
+// ==============================================================================
 const express = require("express");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for the express server
+// ==============================================================================
+
+// Tells node there is an "express" server created
+const app = express();
+
+// Sets an initial port used later in the listener
+const PORT = process.env.PORT || 3001;

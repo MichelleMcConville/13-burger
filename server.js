117f5f00f7b64eb014ae9bfd6cf8592587ba7b7f
Michelle McConville <mchelmconvl@gmail.com>
2020-12-10 14:35:14 -0600

    added middleware

--- server.js
+++ server.js
@@ -14,3 +14,17 @@ const app = express();
 
 // Sets an initial port used later in the listener
 const PORT = process.env.PORT || 3001;
+
+// ==============================================================================
+// MIDDLEWARE - f(x)s executed during the lifecycle of a request to the Express
+// server. Each middleware has access to the HTTP req & res for each route/path
+// it's attached to. (Express itself is compromised wholly of middleware f(x))
+// ==============================================================================
+
+// Making the public folder accessible to the client side
+app.use(express.static("public"));
+
+// Unwrapping client data to make it readable for Express server to use.
+// (stored in req.body)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());

a832353470adc7b61f267512ba4779f952934f10
Michelle McConville <mchelmconvl@gmail.com>
2020-12-10 13:11:25 -0600

    added helper f(x) for SQL syntax

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,16 @@
 // Import MySQL connection
 const connection = require("../config/connection.js");
 
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+      arr.push("?");
+  }
+  return arr.stString();
+}
+
+
 selectAll()
 insertOne()
 updateOne()

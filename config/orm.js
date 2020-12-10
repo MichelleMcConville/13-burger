958cbc9efff921fdefb11718e6de35715a547e91
Michelle McConville <mchelmconvl@gmail.com>
2020-12-10 13:18:51 -0600

    updated orm object

--- config/orm.js
+++ config/orm.js
@@ -12,27 +12,30 @@ function printQuestionMarks(num) {
 
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
-    var arr = [];
-    // Loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-      var value = ob[key];
-      // Check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Turkey Burger => 'Turkey Burger')
-        if (typeof value === "string" && value.indexOf(" ") >=0) {
-          value = "'" + value + "'";
-        }
-        // e.g. {name: 'Turkey Burger'} => ["name='Turkey Burger'"]
-        // e.g. {devoured: true} => ["devoured=true"]
-        arr.push(key + "=" + value);
+  var arr = [];
+  // Loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // Check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Turkey Burger => 'Turkey Burger')
+      if (typeof value === "string" && value.indexOf(" ") >=0) {
+        value = "'" + value + "'";
       }
+      // e.g. {name: 'Turkey Burger'} => ["name='Turkey Burger'"]
+      // e.g. {devoured: true} => ["devoured=true"]
+      arr.push(key + "=" + value);
     }
-    // Translate array of strings to a single comma-separated string
-    return arr.toString();
   }
+  // Translate array of strings to a single comma-separated string
+  return arr.toString();
+}
 
-selectAll()
-insertOne()
-updateOne()
+// Object for all SQL statement f(x)s
+const orm = {
+  selectAll() {},
+  insertOne() {},
+  updateOne() {},
+}
 
 module.exports = orm;

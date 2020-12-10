67e4c7adb1ba6016b09e708e3bb6d004fe67b4bc
Michelle McConville <mchelmconvl@gmail.com>
2020-12-10 12:08:14 -0600

    added baseline connections & blank routers

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model to use its database functions
+const burger = require("../models/burger.js");
+
+// Create all routes & set up logic within those routes where required
+router.get() {};
+
+router.post() {};
+
+router.put() {};
+
+// router.update() {}; ???
+
+// Export routes for server.js to use
+module.exports = router;
+

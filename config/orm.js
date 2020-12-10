6f54b2316e7bb7ac5f1060e987255e938961c0e9
Michelle McConville <mchelmconvl@gmail.com>
2020-12-10 13:13:30 -0600

    added helper f(x) to convert object k/v pairs to SQL syntax

--- config/orm.js
+++ config/orm.js
@@ -10,6 +10,26 @@ function printQuestionMarks(num) {
   return arr.stString();
 }
 
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+    // Loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // Check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Turkey Burger => 'Turkey Burger')
+        if (typeof value === "string" && value.indexOf(" ") >=0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Turkey Burger'} => ["name='Turkey Burger'"]
+        // e.g. {devoured: true} => ["devoured=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+    // Translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
 
 selectAll()
 insertOne()

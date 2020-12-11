1d49f312c687fd339bc5871e1c6086ec088e18ad
Michelle McConville <mchelmconvl@gmail.com>
2020-12-11 09:09:35 -0600

    added router.put

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { end } = require("../config/connection.js");
 
 const router = express.Router();
 
@@ -7,7 +8,7 @@ const burger = require("../models/burger.js");
 
 // Create all routes & set up logic within those routes where required
 router.get("/", (req, res) => {
-  burger.selectAll(function(data) {
+  burger.selectAll(function (data) {
     const hbrObj = { burgers: data };
     console.log(hbrObj);
     res.render("index", hbrObj);
@@ -15,20 +16,20 @@ router.get("/", (req, res) => {
 });
 
 router.post("/api/burger", (req, res) => {
-  burger.insertOne(
-    ["burger_name", "devoured"], 
-    [ req.body.burger_name, req.body.devoured],
-    function(result) {
-      res.json({ id: result.insertID });
-    });
+  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
+    res.json({ id: result.insertID });
+  });
 });
 
-};
-
-router.put() {};
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = "id = " + req.params.id;
+  console.log("condition", condition);
+  burger.updateOne({ devoured: req.body.devoured }, condition, function (result) {
+    if (result.changedRows == 0) { return res.status(404).end(); } else { res.status(200).end(); }
+  });
+});
 
 // router.update() {}; ???
 
 // Export routes for server.js to use
 module.exports = router;
-

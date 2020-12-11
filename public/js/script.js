8d4b08fca5b98893b0563ee1f29fe65fb7e29459
Michelle McConville <mchelmconvl@gmail.com>
2020-12-11 12:08:30 -0600

    format change

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded
 $(function() {
-    
+
   $(".change-devour").on("click", (event) => {
       const id = $(this).data("id");
       const newDevour = $(this).data("newdevour");

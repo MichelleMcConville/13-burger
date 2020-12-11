efe6d8b79f329bce92c3a15a3e1283e6381974f7
Michelle McConville <mchelmconvl@gmail.com>
2020-12-11 09:59:36 -0600

    added delete cb f(x)

--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,22 @@ const orm = require("../config/orm.js");
 
 const burger = {
 
-selectAll(cb) {
-  orm.selectAll("burgers", (res) => { cb(res); });
-},
+  selectAll(cb) {
+    orm.selectAll("burgers", (res) => { cb(res); });
+  },
 
-insertOne(cols, values, cb) {
-  orm.insertOne("burgers", cols, values, (res) => { cb(res); });
-},
+  insertOne(cols, values, cb) {
+    orm.insertOne("burgers", cols, values, (res) => { cb(res); });
+  },
 
-updateOne(objColValues, condition, cb) {
-  orm.updateOne("burgers", objColValues, condition, (res) => { cb(res); });
-},
+  updateOne(objColValues, condition, cb) {
+    orm.updateOne("burgers", objColValues, condition, (res) => { cb(res); });
+  },
 
+  delete(condition, cb) {
+    orm.delete("burgers", condition, (res) => { cb(res); });
+  }
+  
 }
 
 module.exports = burger;

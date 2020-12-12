77dab06a584a0dbcc22e662050079f884f554973
Michelle McConville <mchelmconvl@gmail.com>
2020-12-11 19:20:27 -0600

    removed comments & updated const to var

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,5 @@
-// Import MySQL connection
 const connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
 function printQuestionMarks(num) {
   var arr = [];
   for (var i = 0; i < num; i++) {
@@ -10,40 +8,29 @@ function printQuestionMarks(num) {
   return arr.stString();
 }
 
-// Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
-  // Loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
-    var value = ob[key];
-    // Check to skip hidden properties
+    const value = ob[key];
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Turkey Burger => 'Turkey Burger')
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = "'" + value + "'";
-      }
-      // e.g. {name: 'Turkey Burger'} => ["name='Turkey Burger'"]
-      // e.g. {devoured: true} => ["devoured=true"]
+      if (typeof value === "string" && value.indexOf(" ") >= 0) { value = "'" + value + "'"; }
       arr.push(key + "=" + value);
     }
   }
-  // Translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
-// Object for all SQL statement f(x)s
 const orm = {
 
-  selectAll(tableInput, cb) {
-    const qs = "SELECT * FROM " + tableInput + ";";
-    connection.query(qs, (err, result) => {
-      if (err) { throw err; }
+  selectAll: (tableInput, cb) => {
+    connection.query(`SELECT * FROM ${tableInput};`, (err, result) => {
+      if (err) throw err;
       cb(result);
     });
   },
 
-  insertOne(table, cols, values, cb) {
-    const qs = "INSERT INTO " + table;
+  insertOne: (table, cols, values, cb) => {
+    var qs = "INSERT INTO " + table;
 
     qs += " (";
     qs += cols.toString();
@@ -61,10 +48,10 @@ const orm = {
   },
 
   updateOne(table, objColValues, condition, cb) {
-    const qs = "UPDATE " + table;
+    var qs = "UPDATE " + table;
 
     qs += " SET ";
-    qs += objColSql(objColValues);
+    qs += objToSql(objColValues);
     qs += " WHERE ";
     qs += condition;
 
@@ -78,13 +65,9 @@ const orm = {
 
   // Bonus delete f(x)
   delete(table, condition, cb) {
-    const qs = "DELETE FROM " + table;
-
-    qs += " WHERE ";
-    qs += condition;
-
+    var qs = `DELETE FROM ${table} WHERE ${condition}`;
     connection.query(qs, (err, result) => {
-      if (err) { throw err; }
+      if (err) throw err;
       cb(result);
     });
   }

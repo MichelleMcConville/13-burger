5600c268da2a3c54ec6c0f75ef30b9ab0c805b8b
Michelle McConville <mchelmconvl@gmail.com>
2020-12-14 11:06:38 -0600

    updated router code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,24 +11,24 @@ router.get("/", (req, res) => {
 });
 
 router.post("/api/burgers", (req, res) => {
-  burger.insertOne(req.body.burger_name, (result) => {
+  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], (result) => {
+  //burger.insertOne(req.body.burger_name, (result) => {
     res.json({ id: result.insertId });
   });
 });
 
 router.put("/api/burgers/:id", (req, res) => {
   var condition =  "id = " + req.params.id;
-  console.log("condition", condition); // Do I need this
-  // burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
-  burger.updateOne({ devoured: true }, condition, (result) => {
+  console.log("condition", condition);
+  burger.updateOne({ devoured: req.body.devoured }, condition, (result) => {
     if (result.changedRows == 0) { return res.status(404).end(); } else { res.status(200).end(); }
-  });
+  })
 });
 
 // Bonus delete f(x)
 router.delete("api/burgers/:id", (req, res) => {
   var condition = "id = " + req.params.id;
-  burgers.deleteOne(condition, (result) => {
+  burger.deleteOne(condition, (result) => {
     if (results.effectedRows === 0) {return res.status(404).end(); } else { res.status(200).end(); }
   });
 });

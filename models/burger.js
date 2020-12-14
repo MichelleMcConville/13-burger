4e5ac0b5d42005f76b9d091e9e2085194628f77d
Michelle McConville <mchelmconvl@gmail.com>
2020-12-14 11:21:16 -0600

    added 2ndary f(x)

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,32 @@
 const orm = require("../config/orm.js");
-
 const burger = {
 
   selectAll(cb) {
     orm.selectAll("burgers", (res) => { cb(res); });
   },
 
-  insertOne(burger_name, cb) {
+  insertOne(cols, values, cb) {
+  //2 insertOne(burger_name, cb) {
     console.log(this);
-    orm.insertOne("burgers", ["burger_name", "devoured"], [burger_name, false], cb);
+    orm.insertOne("burgers", cols, values, (res) => { cb(res); });
+    //2 orm.insertOne("burgers", ["burger_name", "devoured"], [burger_name, false], cb);
+    
   },
 
-  updateOne(id, cb) {
-    let condition = `id = ${id}`;
-    orm.updateOne("burgers", {devoured: true}, condition, cb);
+  updateOne(objColValues, condition, cb) {
+  //2 updateOne(id, cb) {
+    //2 let condition = `id = ${id}`;
+    orm.updateOne("burgers", objColValues, condition, (res) => { cb(res) });
+    //2 orm.updateOne("burgers", { devoured: true }, condition, cb);
   },
 
   // Bonus delete f(x)
-  deleteOne(id, cb) {
-    let condition = `id = ${id}`;
-    orm.deleteOne("burgers", condition, (res) => { cb(res); });
-  }
+  deleteOne(objColValues, condition, cb) {
+  //2 deleteOne(id, cb) {
+    //2 let condition = `id = ${id}`;
+    ("burgers", objColValues, condition, (res) => { cb(res); });
+    //2 ("burgers", condition, (res) => { cb(res); });
+  },
 
 }
 

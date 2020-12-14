68dcb47c1eea850bb026bf81f25053a1e2ac0ba2
Michelle McConville <mchelmconvl@gmail.com>
2020-12-14 14:15:27 -0600

    added 2nd option to f(x)script

--- models/burger.js
+++ models/burger.js
@@ -14,17 +14,17 @@ const burger = {
   },
 
   updateOne(objColValues, condition, cb) {
-  //2 updateOne(id, cb) {
-    //2 let condition = `id = ${id}`;
+  // updateOne(id, cb) {
+    // let condition = `id = ${id}`;
     orm.updateOne("burgers", objColValues, condition, (res) => { cb(res) });
-    //2 orm.updateOne("burgers", { devoured: true }, condition, cb);
+    // orm.updateOne("burgers", { devoured: true }, condition, cb);
   },
 
   // Bonus delete f(x)
-  deleteOne(objColValues, condition, cb) {
+  deleteOne(condition, cb) {
   //2 deleteOne(id, cb) {
     //2 let condition = `id = ${id}`;
-    ("burgers", objColValues, condition, (res) => { cb(res); });
+    ("burgers", condition, (res) => { cb(res); });
     //2 ("burgers", condition, (res) => { cb(res); });
   },
 
